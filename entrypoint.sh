a07b8311cd23fc7ec2ec485985d4ae6aaeb80865
Arkan Abuyazid <arkan.2.abuyazid@gmail.com>
2025-01-30 13:18:53 -0800

    Trying out docker environment

---
+++ entrypoint.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e
+
+# Wait for PostgreSQL using environment variables
+until pg_isready -h $DB_HOST -U $DB_USER; do
+  >&2 echo "PostgreSQL is unavailable - sleeping"
+  sleep 1
+done
+
+>&2 echo "PostgreSQL is up - executing command"
+
+# Create and migrate database
+bundle exec rails db:prepare
+
+exec "$@"
