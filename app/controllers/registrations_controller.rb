aa64b6212f069fa7ba0d9deccfb578e898117082
JasonIsAzn <jasonisazn@tamu.edu>
2025-02-08 17:44:41 -0600

    fix account type not being defined

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -8,18 +8,18 @@ class RegistrationsController < ApplicationController
   end
 
   def create
-    account_type = params[:account_type]
-    unless %w[buyer seller].include?(account_type)
+    @account_type = params[:account_type]
+    unless %w[buyer seller].include?(@account_type)
       redirect_to signup_path, alert: "Invalid account type" and return
     end
 
-    user_class = account_type == "buyer" ? Buyer : Seller
+    user_class = @account_type == "buyer" ? Buyer : Seller
     @user = user_class.new(user_params)
 
     if @user.save
       session[:user_id] = @user.id
-      session[:account_type] = account_type
-      redirect_to account_type == "buyer" ? buyer_dashboard_path : seller_dashboard_path, notice: "Welcome, #{account_type.capitalize}!"
+      session[:account_type] = @account_type
+      redirect_to @account_type == "buyer" ? buyer_dashboard_path : seller_dashboard_path, notice: "Welcome, #{@account_type.capitalize}!"
     else
       flash[:alert] = @user.errors.full_messages.to_sentence
       render :new, status: :unprocessable_entity
