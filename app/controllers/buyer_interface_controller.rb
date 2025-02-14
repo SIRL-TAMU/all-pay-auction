bb2831d5bb2d49a71b6c5f4a4f79f29c43b61461
SteveWang110 <stevewang828@gmail.com>
2025-02-14 15:04:39 -0600

    added tests for code coverage 100%

--- app/controllers/buyer_interface_controller.rb
+++ app/controllers/buyer_interface_controller.rb
@@ -2,7 +2,6 @@
 
 # Controller to manage the interface for buyers.
 class BuyerInterfaceController < ApplicationController
-  before_action :require_login
   before_action :ensure_buyer
 
   def index
@@ -17,6 +16,8 @@ class BuyerInterfaceController < ApplicationController
     #   Rails.logger.debug "are you a buyer"
 
     # end
-    redirect_to login_path, alert: I18n.t("alerts.access_denied") unless buyer? # TODO: test later
+    return if logged_in? && buyer?
+
+      redirect_to root_path, alert: I18n.t("alerts.access_denied")
   end
 end
