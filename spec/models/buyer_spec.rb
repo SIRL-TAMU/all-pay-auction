0408865ed4b1f40956d02bf25a5a266e9911115d
JasonIsAzn <jasonisazn@tamu.edu>
2025-02-24 17:56:58 -0600

    fix rubocop offenses

--- spec/models/buyer_spec.rb
+++ spec/models/buyer_spec.rb
@@ -4,35 +4,27 @@ require 'rails_helper'
 
 RSpec.describe Buyer, type: :model do
   describe "validations" do
+    let(:valid_attributes) do
+      { first_name: "John", last_name: "Doe", email: "buyer1@gmail.com", password: "test123", amount: 0 }
+    end
+
     it "is valid with valid attributes" do
-      buyer = Buyer.new(first_name: "John",
-                        last_name: "Doe",
-                        email: "buyer1@gmail.com",
-                        password: "test123",
-                        amount: 0)
+      buyer = Buyer.new(valid_attributes)
       expect(buyer).to be_valid
     end
+
     it "is invalid without an email" do
-      buyer = Buyer.new(first_name: "John",
-                        last_name: "Doe",
-                        password: "test123",
-                        amount: 0)
+      buyer = Buyer.new(valid_attributes.except(:email))
       expect(buyer).to_not be_valid
     end
-    it "is invalid with duplicated email" do
-      buyer1 = Buyer.create(first_name: "John",
-                            last_name: "Doe",
-                            email: "buyer1@gmail.com",
-                            password: "test123",
-                            amount: 0)
 
-      buyer2 = Buyer.new(first_name: "Jane",
-                         last_name: "Doe",
-                         email: "buyer1@gmail.com",
-                         password: "test123",
-                         amount: 0)
-      expect(buyer1).to be_valid
-      expect(buyer2).to_not be_valid
+    context "when email is duplicated" do
+      before { Buyer.create!(valid_attributes) }
+
+      it "is invalid with a duplicate email" do
+        buyer2 = Buyer.new(valid_attributes.merge(first_name: "Jane"))
+        expect(buyer2).to_not be_valid
+      end
     end
   end
 end
