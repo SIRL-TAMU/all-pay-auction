0609fba2e72ef7dbf5221de1193f33770ff1b139
JasonIsAzn <jasonisazn@tamu.edu>
2025-02-23 20:23:46 -0600

    implement auction block design

--- app/helpers/auction_items_helper.rb
+++ app/helpers/auction_items_helper.rb
@@ -2,4 +2,18 @@
 
 # Helper methods for Auction Items
 module AuctionItemsHelper
+  def auction_status(item)
+    now = Time.zone.now
+    if item.opening_date > now
+      return ['upcoming', "Opens on #{item.opening_date.strftime('%b %d, %Y')}"]
+    elsif item.closing_date < now
+      return ['closed', "Closed on #{item.closing_date.strftime('%b %d, %Y')}"]
+    else
+      remaining_time = item.closing_date - now
+      days = (remaining_time / 1.day).to_i
+      hours = ((remaining_time % 1.day) / 1.hour).to_i
+      minutes = ((remaining_time % 1.hour) / 1.minute).to_i
+      return ['active', "Closing in #{days}d #{hours}h #{minutes}m"]
+    end
+  end
 end
