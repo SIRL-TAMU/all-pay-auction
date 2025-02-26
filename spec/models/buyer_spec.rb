fa7caa5ae09f37b377e0347ab8b344b109f66890
Praewa Pitiphat <riolu_san@tamu.edu>
2025-02-26 16:05:07 -0600

    added more buyer spec test

--- spec/models/buyer_spec.rb
+++ spec/models/buyer_spec.rb
@@ -27,4 +27,48 @@ RSpec.describe Buyer, type: :model do
       end
     end
   end
+
+  describe "associations" do
+    it "has many bids" do
+      association = Buyer.reflect_on_association(:bids)
+      expect(association).to_not be_nil
+      expect(association.macro).to eq(:has_many)
+      expect(association.options[:dependent]).to eq(:destroy)
+    end
+    it "has many transaction" do
+      association = Buyer.reflect_on_association(:transactions)
+      expect(association).to_not be_nil
+      expect(association.macro).to eq(:has_many)
+      expect(association.options[:dependent]).to eq(:destroy)
+    end
+    it "has many auction_items through bids" do
+      association = Buyer.reflect_on_association(:auction_items)
+      expect(association).to_not be_nil
+      expect(association.macro).to eq(:has_many)
+      expect(association.options[:through]).to eq(:bids)
+    end
+  end
+
+  describe "custom methods" do
+    describe "#sufficient_funds?" do
+      it "returns true if buyer has sufficient funds" do
+        buyer = Buyer.create(first_name: "John", last_name: "Doe", email: "buyer1@gmail.com", password: "test1", amount: 100)
+        auction_item = AuctionItem.create(name: "item1", max_bid: "10")
+        expect(buyer.sufficient_funds?(20, auction_item)).to be(true)
+      end
+      it "returns false if buyer does not have sufficient funds" do
+        buyer = Buyer.create(first_name: "John", last_name: "Doe", email: "buyer1@gmail.com", password: "test1", amount: 100)
+        auction_item = AuctionItem.create(name: "item1", max_bid: "100")
+        expect(buyer.sufficient_funds?(120, auction_item)).to be(false)
+      end
+    end
+
+    describe "#deduct_funds" do
+      it "deduct funds from buyer account after places bid" do
+        buyer = Buyer.create(first_name: "John", last_name: "Doe", email: "buyer1@gmail.com", password: "test1", amount: 100)
+        buyer.deduct_funds(10)
+        expect(buyer.amount).to eq(90)
+      end
+    end
+  end
 end
