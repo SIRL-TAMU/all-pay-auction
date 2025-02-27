3e25c5e5a7f572c692a3734d8fccec1d13edc177
Arkan Abuyazid <arkan.2.abuyazid@gmail.com>
2025-02-27 15:15:44 -0800

    Updated seeds.rb

--- db/migrate/20250225015300_update_auction_items.rb
+++ db/migrate/20250225015300_update_auction_items.rb
@@ -1,6 +1,6 @@
 class UpdateAuctionItems < ActiveRecord::Migration[7.2]
   def change
-    add_column :auction_items, :innate_value, :decimal, null: false
+    add_column :auction_items, :innate_value, :decimal, null: false 
     add_column :auction_items, :min_increment, :decimal, null: false
     add_column :auction_items, :is_archived, :boolean, default: false, null: false
 
