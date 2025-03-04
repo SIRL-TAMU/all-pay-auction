ec54522801ff4dcf42b9ee12f14f78968c866f05
Arkan Abuyazid <arkan.2.abuyazid@gmail.com>
2025-03-04 14:44:03 -0800

    updated buyer and auction_items records

--- app/models/auction_item.rb
+++ app/models/auction_item.rb
@@ -4,6 +4,7 @@
 class AuctionItem < ApplicationRecord
   belongs_to :seller
   has_many :bids, dependent: :destroy
+  belongs_to :winning_buyer, class_name 'Buyer', optional: true
 
   def max_bid
     bids.maximum(:amount) || self[:curr_max_bid] || 0 # here max_bid could be starting price
