ba6fc8bfe0ab70223d5a1f0377acff33c56fb6b7
JasonIsAzn <jasonisazn@tamu.edu>
2025-03-06 16:52:19 -0600

    remove temp dev config

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,8 +1,5 @@
 OmniAuth.config.allowed_request_methods = %i[get post]
 
-# Temporarily skip CSRF protection for OmniAuth
-OmniAuth.config.request_validation_phase = proc { }
-
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2, "324405924578-kmc64rb6lqsv7h1anhkbvarg2dk12bv3.apps.googleusercontent.com",
            "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"
