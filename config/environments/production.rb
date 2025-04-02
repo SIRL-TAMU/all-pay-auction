1d66ba6b92dc3ec2741fb2987443a24bf9ff6647
Praewa Pitiphat <riolu_san@tamu.edu>
2025-04-02 09:14:51 -0500

    Enable uglifier harmony mode

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,7 +28,9 @@ Rails.application.configure do
   # Compress CSS using a preprocessor.
   # config.assets.css_compressor = :sass
 
-  config.assets.js_compressor = :uglifier
+  # config.assets.js_compressor = :uglifier
+  config.assets.js_compressor = Uglifier.new(harmony: true)
+
   # Do not fall back to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
 
