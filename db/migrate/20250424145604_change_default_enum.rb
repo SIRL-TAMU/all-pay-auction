bf662b99e5870721b169b2558f65bdc87bdd4445
SteveWang110 <stevewang828@gmail.com>
2025-04-24 12:23:47 -0500

    fixed migration error, auction_items instead of auction_item

--- db/migrate/20250424145604_change_default_enum.rb
+++ db/migrate/20250424145604_change_default_enum.rb
@@ -1,5 +1,5 @@
 class ChangeDefaultEnum < ActiveRecord::Migration[7.2]
   def change
-    change_column_default :auction_item, :item_type, 0
+    change_column_default :auction_items, :item_type, 0
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_04_23_200837) do
+ActiveRecord::Schema[7.2].define(version: 2025_04_24_145604) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
