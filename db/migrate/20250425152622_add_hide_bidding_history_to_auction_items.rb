f6352742c68efbf9c7cb7a5c67176e9ec5f92ff5
SteveWang110 <stevewang828@gmail.com>
2025-04-25 12:23:38 -0500

    balance under my account

--- db/migrate/20250425152622_add_hide_bidding_history_to_auction_items.rb
+++ db/migrate/20250425152622_add_hide_bidding_history_to_auction_items.rb
@@ -1,5 +1,7 @@
 class AddHideBiddingHistoryToAuctionItems < ActiveRecord::Migration[7.0]
   def change
-    add_column :auction_items, :hide_bidding_history, :boolean, default: false, null: false
+    unless column_exists?(:auction_items, :hide_bidding_history)
+      add_column :auction_items, :hide_bidding_history, :boolean, default: false, null: false
+    end
   end
 end
